a7ee564ae8257445a9fced9c31c3c1187cba6b7f
Kevin Riquena <riquena969@gmail.com>
2018-06-09 20:51:06 -0300

    Improved start ball position

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ var width     = 750;
 var height    = 350;
 var heightBar = parseInt(height/4);
 var widthBar  = 10;
+var ballWidth = 22;
 
-var ball   = new Ball(50, 50, 5, 2, 22);
+var ball   = new Ball(widthBar, (height/2)-(ballWidth/2), 5, 2, ballWidth);
 var lbar   = new Bar(3,                (height/2)-(heightBar/2), widthBar, heightBar);
 var rbar   = new Bar(width - widthBar - 3, (height/2)-(heightBar/2), widthBar, heightBar);
 
