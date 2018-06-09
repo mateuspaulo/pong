ce427ff776983a516abc2dce97a188c74d2442fd
Kevin Riquena <riquena969@gmail.com>
2018-06-09 20:34:34 -0300

    Improved verification of position of the ball in the panel

--- js/ball.js
+++ js/ball.js
@@ -26,23 +26,27 @@ class Ball {
         this.x += this.speedX;
         this.y += this.speedY;
 
-        this.element.style.left   = this.x + 'px';
-        this.element.style.top    = this.y + 'px';
-
         if (this.x >= width - this.raio) {
             this.speedX *= -1;
+            this.x = width - this.raio;
         }
 
         if (this.y >= height - this.raio) {
             this.speedY *= -1;
+            this.y = height - this.raio;
         }
 
         if (this.x <= 0) {
             this.speedX *= -1;
+            this.x = 0;
         }
 
         if (this.y <= 0) {
             this.speedY *= -1;
+            this.y = 0;
         }
+
+        this.element.style.left   = this.x + 'px';
+        this.element.style.top    = this.y + 'px';
     }
 }
